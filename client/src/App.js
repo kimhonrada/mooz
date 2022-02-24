0fabf4bca6a89c992eaeabbb4e5429b7b2e342ea
Kim Honrada <kimhonrada21@gmail.com>
2022-02-24 14:55:01 -0500

    Add initial component jsx files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <AppBar position='static' color='inherit'>
+          <Typography variant='h2' align='center'>
+            Mooz
+          </Typography>
+        </AppBar>
+        {/* video player*/}
+        {/* options */}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
