fef8888d4dc4a863799c85b5131761fa42354339
Kim Honrada <kimhonrada21@gmail.com>
2022-02-24 15:16:48 -0500

    Add initial App style

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
 import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import VideoPlayer from "./components/VideoPlayer.jsx";
+import Notifications from "./components/Notifications.jsx";
+import Options from "./components/Options.jsx";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <>
-      <div>
-        <AppBar position='static' color='inherit'>
-          <Typography variant='h2' align='center'>
-            Mooz
-          </Typography>
-        </AppBar>
-        {/* video player*/}
-        {/* options */}
-      </div>
-    </>
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position='static' color='primary'>
+        <Typography variant='h2' align='center'>
+          Mooz
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
   );
 };
 
